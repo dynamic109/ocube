9057372c4b13497f613c71eaae6de291527c57bd
Derickdavid2002 <isiborderick4@gmail.com>
2025-03-31 14:57:37 +0100

    update

---
+++ src/components/landing-page/HeroPrimary.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+
+function HeroPrimary() {
+  return <div className="max-w-[1400px] mx-auto">
+
+    <div className="bg-[rgb(243,244,246)] mt-5 pt-7">
+      <h2 className="text-3xl font-bold flex justify-center items-center mt-10 ">Further your development with <br />
+      our incredible features.</h2>
+    <div className="flex py-4 justify-center items-center" >
+      <div className="mt-3">
+      <img src="/Updated-Content-icon.svg" alt="" />
+    <p className="text-1xl font-bold">Update Content</p>
+<p>A prime benefit of learning online is that it <br />
+makes sure that you are in synchronization <br />
+with modern learners. This enables the <br />
+learner to access updated content 
+whenever they want it.</p>
+
+<img src="/Consistency-icon.svg" alt="" />
+    <p className="text-1xl font-bold mt-3">Consistency</p>
+<p>eLearning enables educators to get a higher <br />
+degree of coverage to communicate the <br />
+message in a consistent way for their <br />
+target audience. This ensures that <br />
+all learners receive the same training.</p>
+
+      </div>
+<div><img className="w-[420px] h-[520px] mt-3"src="/Further-Image.svg" alt="" /></div>
+
+<div className="mt-3">
+<img src="/Scalability-icon.svg" alt="" />
+    <p className="text-1xl font-bold">Scalability</p>
+<p>eLearning helps in creating and <br />
+communicating new training, policies. <br />
+Whether it is for formal education or <br />
+entertainment, eLearning is very <br />
+quick way of learning!.</p>
+<div>
+<img src="/Reduced Costs.svg" alt="" />
+    <p className="text-1xl font-bold mt-3">Reduced Cost</p>
+<p>eLearning is cost effective as compared <br />
+to traditional forms of learning. The <br />
+reason for this price reduction is <br />
+because learning through this <br />
+mode happens quickly and easily. </p>
+</div>
+
+    
+</div>
+    </div>
+
+
+</div>
+
+
+
+
+  </div>
+}
+
+export default HeroPrimary;
--- src/components/landing-page/landing.jsx
+++ src/components/landing-page/landing.jsx
@@ -1,9 +1,14 @@
 import Header from "../header";
-
+import Hero from "./hero";
+import HeroPrimary from "./HeroPrimary"
+import HeroSecondary from "./HeroSecondary"
 function LandingPage() {
   return (
-    <div className="max-w-[1400px] ">
+    <div className="max-w-[1400px] mx-auto ">
       <Header />
+      <Hero />
+      <HeroPrimary />
+      <HeroSecondary />
     </div>
   );
 }
