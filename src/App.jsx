37a856be2b6dd639659dde540a86c3a141eab53b
tamilore <oluwatamilorevirtious@gmail.com>
2025-03-31 20:37:20 +0100

    dashlogin/sigin

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
 import React from "react";
+import { BrowserRouter, Route, Router, } from "react-router-dom";
+import { Routes } from "react-router"
 import LandingPage from "./components/landing-page/landing";
-
+import Dashlogin from "./components/dashboard/Dashlogin";
+import Signin from "./components/dashboard/Signin";
+ import Resources from "./components/dashboard/Resources";
 function App() {
   return (
     <div>
-      <LandingPage />
       
+      
+      <BrowserRouter>
+      
+        <Routes>
+          <Route path="/"  element={<LandingPage/>}/>
+        <Route path="/Dashlogin" element={<Dashlogin />} />
+        <Route path="/Signin" element={<Signin/>} /> 
+        <Route path="/Resources" element={<Resources/>} /> 
+        
+          </Routes>
+      </BrowserRouter>
+       
     </div>
   );
 }
---
+++ src/components/dashboard/Resources.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+// import Logo from "/Logo.png";
+import SideBar from "./SideBar";
+const Resources= () => {
+    return( 
+        <> <div><SideBar /></div></>
+    )
+};
+export default Resources;
